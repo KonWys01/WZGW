ce8e77c2ab95901ad580e17e252c48096587ac8b
KonWys01 <konradwysokinski01@gmail.com>
2022-01-18 19:45:25 +0100

    from 92

--- zad_4/main.py
+++ zad_4/main.py
@@ -113,6 +113,42 @@ def GK_to_2000(A):
     return x_2000, y_2000, x, y
 
 
+def from_92(x_92, y_92, l0=19):
+    A_0 = 1 - e2 / 4 - (3 * (e2 ** 2)) / 64 - (5 * (e2 ** 3)) / 256
+    A_2 = 3 / 8 * (e2 + (e2 ** 2) / 4 + (15 * (e2 ** 3)) / 128)
+    A_4 = 15 / 256 * (e2 ** 2 + 3 * (e2 ** 3) / 4)
+    A_6 = 35 / 3072 * (e2 ** 3)
+
+    l0 = np.deg2rad(l0)
+
+    m = 0.9993
+    x = (x_92 + 5300000) / m
+    y = (y_92 - 500000) / m
+
+    phi = x_92 / (a*A_0)
+
+    while True:
+        sigma = a*(A_0*phi - A_2*np.sin(2*phi) + A_4*np.sin(4*phi) - A_6*np.sin(6*phi))
+        phi_new = phi + (x_92 - sigma)/(a*A_0)
+        if np.fabs(phi_new - phi) < np.deg2rad(0.000001 / 3600):
+            break
+        else:
+            phi = phi_new
+
+    N = a / (np.sqrt(1-e2*(np.sin(phi)**2)))
+    M = a*(1-e2)/(np.sqrt((1-e2*np.sin(phi)**2)**3))
+    t = np.tan(phi)
+    n2 = e2_prim * (np.cos(phi) ** 2)
+
+    old_phi = phi
+    phi = old_phi - ((y_92**2)*t)*(1 - ((y_92**2)/(12*N))*(5 + 3*t**2 + n2 - 0*n2*(t**2) - 4*n2**2) + ((y_92**4)/(360*N**4))*(61 + 90*t**2 + 45*t**4))/(2*M*N)
+    lam = l0 + (y_92/(N*np.cos(old_phi)))*(1 - (y**2/(6*N**2))*(1 + 2*t**2 + n2) + (y**4/(120*N**4))*(5 + 28*t**2 + 24*t**4 + 6*n2 + 8*n2*t**2))
+
+    A = [np.degrees(i) for i in [phi, lam]]
+    return A
+
+
+
 print(GK_to_1992(A, 19))
 print(GK_to_1992(A))
 print(GK_to_2000(A))
